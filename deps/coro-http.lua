f3f93cec584515c5a99a265e1afba8a41ea7dc8c
Bilal Bassam <38175840+Bilal2453@users.noreply.github.com>
2023-07-21 03:51:16 +0300

    coro-http: attempt fixing relative URIs

--- deps/coro-http.lua
+++ deps/coro-http.lua
@@ -15,6 +15,18 @@
 local httpCodec = require('http-codec')
 local net = require('coro-net')
 
+local connections = {}
+
+local REDIRECTION_CODES = {
+  [301] = true,
+  [302] = true,
+  [307] = true,
+}
+local REDIRECTION_METHODS = {
+  HEAD = true,
+  GET = true,
+}
+
 local function createServer(host, port, onConnect)
   return net.createServer({
     host = host,
@@ -43,21 +55,21 @@ local function createServer(host, port, onConnect)
 end
 
 local function parseUrl(url)
-  local protocol, host, hostname, port, path = url:match("^(https?:)//(([^/:]+):?([0-9]*))(/?.*)$")
+  local protocol, host, hostname, port, path, hash = url:match("^(https?:)//(([^/:#]+):?([0-9]*))(/?[^#]*)(#?.*)$")
   if not protocol then error("Not a valid http url: " .. url) end
   local tls = protocol == "https:"
   port = port and tonumber(port) or (tls and 443 or 80)
   if path == "" then path = "/" end
+  if hash == "" then hash = nil end
   return {
     tls = tls,
     host = host,
     hostname = hostname,
     port = port,
-    path = path
+    path = path,
+    hash = hash,
   }
 end
-
-local connections = {}
 
 local function getConnection(host, port, tls, timeout)
   for i = #connections, 1, -1 do
@@ -103,36 +115,79 @@ local function saveConnection(connection)
   connection.socket:unref()
 end
 
-local function request(method, url, headers, body, customOptions)
-  -- customOptions = { timeout = number, followRedirects = boolean }
-  local options = {}
-  if type(customOptions) == "number" then
-    -- Ensure backwards compatibility, where customOptions used to just be timeout
-    options.timeout = customOptions
+---@param options number | {timeout: number, followRedirects: boolean, redirectionCodes: {[integer]: boolean}, redirectionMethods: {[string]: boolean}}
+local function normalizeRequest(options)
+  local optionsType = type(options)
+  if optionsType == "number" then
+    options.timeout = options
+  elseif optionsType == "table" then
+    options = options
   else
-    options = customOptions or {}
+    options = {}
   end
-  options.followRedirects = options.followRedirects == nil and true or options.followRedirects -- Follow any redirects, Default: true
 
+  if options.followRedirects == nil or options.followRedirects then
+    options.followRedirects = true
+  else
+    options.followRedirects = false
+  end
+
+  if type(options.redirectionCodes) == "table" then
+    options.redirectionCodes = options.redirectionCodes
+  else
+    options.redirectionCodes = REDIRECTION_CODES
+  end
+
+  if type(options.redirectionMethods) == "table" then
+    options.redirectionMethods = options.redirectionMethods
+  else
+    options.redirectionMethods = REDIRECTION_METHODS
+  end
+
+  return options
+end
+
+local function resolveRelativePath(basePath, relativePath)
+  -- TODO
+end
+
+local function resolveLocation(baseUri, relativeUrl)
+  local uri = parseUrl(relativeUrl)
+  uri.hostname = uri.hostname or baseUri.hostname
+  uri.host = uri.host or baseUri.host
+  uri.hash = uri.hash or baseUri.hash
+  uri.path = resolveRelativePath(baseUri.path, uri.path)
+  if uri.tls ~= baseUri.tls then
+    uri.tls = uri.tls
+  end
+  -- TODO: figure out uri.port
+  uri.hash = uri.hash or baseUri.hash
+  return string.format("http%s://%s:%d%s%s", uri.tls and "s" or "", uri.host, uri.port, uri.path, uri.hash or "")
+end
+
+local function request(method, url, headers, body, options)
+  -- resolve the options
+  options = normalizeRequest(options)
+
+  -- parse the url and establish a connection (or retrive the cached one)
   local uri = parseUrl(url)
   local connection = getConnection(uri.hostname, uri.port, uri.tls, options.timeout)
-  local read = connection.read
-  local write = connection.write
+  local read, write = connection.read, connection.write
 
+  -- construct the request structure
   local req = {
     method = method,
     path = uri.path,
   }
-  local contentLength
-  local chunked
+  -- gather info about the provided headers
+  local contentLength, chunked
   local hasHost = false
   if headers then
     for i = 1, #headers do
-      local key, value = unpack(headers[i])
-      key = key:lower()
+      local key = headers[i][1]:lower()
       if key == "content-length" then
-        contentLength = value
-      elseif key == "content-encoding" and value:lower() == "chunked" then
+        contentLength = headers[i][2]
+      elseif key == "content-encoding" and headers[i][2]:lower() == "chunked" then
         chunked = true
       elseif key == "host" then
         hasHost = true
@@ -140,19 +195,20 @@ local function request(method, url, headers, body, customOptions)
       req[#req + 1] = headers[i]
     end
   end
+
+  -- if no Host header, default to uri.host
   if not hasHost then
     req[#req + 1] = {"Host", uri.host}
   end
-
-
-  if type(body) == "string" then
-    if not chunked and not contentLength then
-      req[#req + 1] = {"Content-Length", #body}
-    end
+  -- if body is provided but not a Content-Length header, automatically supply one
+  if type(body) == "string" and not (chunked and contentLength) then
+    req[#req + 1] = {"Content-Length", #body}
   end
 
+  -- send the request
   write(req)
   if body then write(body) end
+  -- receive the response
   local res = read()
   if not res then
     if not connection.socket:is_closing() then
@@ -167,10 +223,13 @@ local function request(method, url, headers, body, customOptions)
     error("Connection closed")
   end
 
+  -- receive and construct the response body payload
   body = {}
   if req.method == "HEAD" then
+    -- if the method is HEAD, there is no body to receive
     connection.reset()
   else
+    -- receive the body chunks until reaching end of stream 
     while true do
       local item = read()
       if not item then
@@ -183,24 +242,28 @@ local function request(method, url, headers, body, customOptions)
       body[#body + 1] = item
     end
   end
+  body = table.concat(body)
 
+  -- cache the connection if keepAlive is presented
+  -- otherwise send end of stream and (eventually) close it
   if res.keepAlive then
     saveConnection(connection)
   else
     write()
   end
 
-  -- Follow redirects
-  if method == "GET" and (res.code == 302 or res.code == 307) and options.followRedirects then
+  -- automatically follow some redirects
+  if options.redirectionMethods[method] and options.followRedirects and options.redirectionCodes[res.code] then
     for i = 1, #res do
-      local key, location = unpack(res[i])
-      if key:lower() == "location" then
+      local key = res[i][1]:lower()
+      if key == "location" then
+        local location = resolveLocation(uri, res[i][2])
         return request(method, location, headers)
       end
     end
   end
 
-  return res, table.concat(body)
+  return res, body
 end
 
 return {
